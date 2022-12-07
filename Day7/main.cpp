e2b4f23e48ac0ccdf2fde949edf953b197486a95
Josh Gaston <jgaston93@gmail.com>
2022-12-07 07:41:14 -0600

    day 7 initial commit

--- Day7/main.cpp
+++ Day7/main.cpp
@@ -3,53 +3,68 @@
 #include <cstdlib>
 #include <cstring>
 
-bool CheckDuplicates(char* sub_stream, int sub_stream_size);
+struct File
+{
+    char* name = 0;
+    int size = 0;
+};
+
+struct Directory
+{
+    char* name = 0;
+    Directory* parent_directory = 0;
+    int num_directories = 0;
+    int num_files = 0;
+    Directory** directories = 0;
+    File** files = 0;
+};
 
 int main()
 {
     std::ifstream input_file("input.txt");
-    char buffer[4096];
-    input_file.getline(buffer, sizeof(buffer));
-    int stream_size = strlen(buffer);
+    char buffer[64];
     
-    int marker_position = 0;
-    const int marker_size = 14;
+    input_file.getline(buffer, sizeof(buffer));
 
-    for(int i = 0; i < stream_size - marker_size; i++)
+    struct Directory* root = new Directory();
+    root->name = strdup("/");
+
+    struct Directory* current_dir = root;
+
+    while(input_file.peek() != EOF)
     {
-        if(!CheckDuplicates(&buffer[i], marker_size))
+        input_file.getline(buffer, sizeof(buffer));
+
+        if(buffer[2] == 'l')
         {
-            marker_position = i + marker_size;
-            break;
+            int num_directories = 0;
+            int num_files = 0;
+            struct Directory** directories = new Directory*[64];
+            struct File** files = new File*[64];
+            while(input_file.peek() != '$')
+            {
+                input_file.getline(buffer, sizeof(buffer));
+                if(buffer[0] == 'd')
+                {
+                    struct Directory* directory = new Directory();
+                    directory->name = strdup(&buffer[4]);
+                    directory->parent_directory = current_dir;
+                    directories[num_directories++] = directory;
+                }
+                else
+                {
+                    struct File* file = new File();
+
+                }
+            }
+        }
+        else if(buffer[2] == 'c')
+        {
+
         }
     }
-
-    printf("Marker Position: %d\n", marker_position);
 
     input_file.close();
 
     return 0;
-}
-
-
-bool CheckDuplicates(char* sub_stream, int sub_stream_size)
-{
-    bool duplicate_found = false;
-
-    for(int i = 0; i < sub_stream_size - 1; i++)
-    {
-        for(int j = i + 1; j < sub_stream_size; j++)
-        {
-            if(sub_stream[i] == sub_stream[j])
-            {
-                duplicate_found = true;
-            }
-        }
-        if(duplicate_found)
-        {
-            break;
-        }
-    }
-
-    return duplicate_found;
 }
